3f13d62eb7f1a9a5464d0b6704b43466bd51ccc0
stangen <31017977+stangen@users.noreply.github.com>
2018-06-21 16:58:18 -0700

    Add files via upload

---
+++ duplicate_madaus/import_shell_var.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 15:17:41 2018
+
+@author: stangen
+"""
+
+import sys
+
+var1 = sys.argv[1]
+var2 = sys.argv[2].split(',')
+var3 = sys.argv[3]
+var4 = sys.argv[4]
+
+print(var1)
+print(len(var1))
+print(type(var1))
+
+#var2 = var2.split(',')
+print(var2)
+print(len(var2))
+print(type(var2))
+
+print(var3)
+print(len(var3))
+print(type(var3))
+
+print(var4)
+print(type(var4))
