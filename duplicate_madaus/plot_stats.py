eee818ecd672a5cad5fb39db200f45251d95372b
stangen <31017977+stangen@users.noreply.github.com>
2018-06-27 15:45:25 -0700

    Add files via upload

--- duplicate_madaus/plot_stats.py
+++ duplicate_madaus/plot_stats.py
@@ -13,7 +13,7 @@ import efa_functions as ef
 
 
 start_date = datetime(2013,4,1,0)
-end_date = datetime(2013,4,1,12)
+end_date = datetime(2013,6,30,12)
 
 variables = ['T2M','ALT']
 
@@ -23,6 +23,8 @@ var_units = {
             }
 
 filedir = '/home/disk/hot/stangen/Documents/EFA/duplicate_madaus/mse_var_output/'
+
+savedir = '/home/disk/hot/stangen/Documents/EFA/duplicate_madaus/plots/'
 
 #create strings for loading txt file containing the stats
 sy = start_date.strftime('%Y')
@@ -45,7 +47,7 @@ f1 = open(filepath, 'r')
 stats = f1.readlines()
 
 stats_dict = {}
-#build the dictionary from each entry in the statistics file
+
 for line in stats:
     line_split = line.split(',')
     efa = line_split[0] #posterior or prior
@@ -57,27 +59,58 @@ for line in stats:
     errvar = line_split[6] #error variance
     #this block is to set up the dictionary structure
     stats_dict[var] = stats_dict.get(var,{})
-    stats_dict[var][ens] = stats_dict.get(ens, {})
-    stats_dict[var][ens][efa] = stats_dict.get(efa, {})
+    stats_dict[var][ens] = stats_dict[var].get(ens, {})
+    stats_dict[var][ens][efa] = stats_dict[var][ens].get(efa, {})
     stats_dict[var][ens][efa]['Forecast_Hour'] = stats_dict[var][ens][efa].get('Forecast_Hour',[])
     stats_dict[var][ens][efa]['MSE'] = stats_dict[var][ens][efa].get('MSE',[])
     stats_dict[var][ens][efa]['Ensemble_Variance'] = stats_dict[var][ens][efa].get('Ensemble_Variance',[])
     stats_dict[var][ens][efa]['Error_Variance'] = stats_dict[var][ens][efa].get('Error_Variance',[])
-#now fill the dictionary with the statistics
-for line in stats:
-    line_split = line.split(',')
-    efa = line_split[0] #posterior or prior
-    ens = line_split[1] #ensemble type
-    var = line_split[2] #observation variable type
-    fh = line_split[3] #forecast hour
-    mse = line_split[4] #mean squared error
-    variance = line_split[5] #variance
-    errvar = line_split[6] #error variance
     
     stats_dict[var][ens][efa]['Forecast_Hour'].append(int(fh))
     stats_dict[var][ens][efa]['MSE'].append(float(mse))
     stats_dict[var][ens][efa]['Ensemble_Variance'].append(float(variance))
     stats_dict[var][ens][efa]['Error_Variance'].append(float(errvar))
-    
-    
+
+for v in stats_dict:
+    for e in stats_dict[v]:
+        for E in stats_dict[v][e]:
+            #convert each array to a numpy array so it can be sorted
+            stats_dict[v][e][E]['Forecast_Hour'] = np.array(stats_dict[v][e][E]['Forecast_Hour'])
+            stats_dict[v][e][E]['MSE'] = np.array(stats_dict[v][e][E]['MSE'])
+            stats_dict[v][e][E]['Ensemble_Variance'] = np.array(stats_dict[v][e][E]['Ensemble_Variance'])
+            stats_dict[v][e][E]['Error_Variance'] = np.array(stats_dict[v][e][E]['Error_Variance'])
+            
+            #sort each array in order of ascending forecast hour
+            sorted_ind = stats_dict[v][e][E]['Forecast_Hour'].argsort()
+            stats_dict[v][e][E]['Forecast_Hour'] = stats_dict[v][e][E]['Forecast_Hour'][sorted_ind]
+            stats_dict[v][e][E]['MSE'] = stats_dict[v][e][E]['MSE'][sorted_ind]
+            stats_dict[v][e][E]['Ensemble_Variance'] = stats_dict[v][e][E]['Ensemble_Variance'][sorted_ind]
+            stats_dict[v][e][E]['Error_Variance'] = stats_dict[v][e][E]['Error_Variance'][sorted_ind]
+            
+            
+#plotting variables
+stats_list = ['MSE','Ensemble_Variance']
+#now plot 
+for v in variables:
+    for s in stats_list:
+        fig = plt.figure(figsize=(14,8))
+        
+        plt.plot(stats_dict[v]['ecmwf']['prior']['Forecast_Hour'],stats_dict[v]['ecmwf']['prior'][s],
+                 linestyle='dashed',marker='o',color='r',label='Original ECMWF')
+        plt.plot(stats_dict[v]['ecmwf']['posterior']['Forecast_Hour'],stats_dict[v]['ecmwf']['posterior'][s],
+                 marker='o',color='r',label='Updated ECMWF')
+        plt.plot(stats_dict[v]['eccc']['prior']['Forecast_Hour'],stats_dict[v]['eccc']['prior'][s],
+                 linestyle='dashed',marker='o',color='k',label='Original CMC')
+        plt.plot(stats_dict[v]['eccc']['posterior']['Forecast_Hour'],stats_dict[v]['eccc']['posterior'][s],
+                 marker='o',color='k',label='Updated CMC')   
+        
+        plt.xticks(np.arange(min(stats_dict[var]['ecmwf']['prior']['Forecast_Hour'])-6, 
+                             max(stats_dict[var]['ecmwf']['prior']['Forecast_Hour'])+6, 6))
+        plt.grid()
+        plt.legend(loc = 'upper left')
+        plt.title(v+' '+s,fontsize=20)
+        plt.xlabel('Forecast Hour',fontsize=14)
+        plt.ylabel(var_units[v],fontsize=14)
+        
+        fig.savefig(savedir+v+'_'+s+'.png')
     
