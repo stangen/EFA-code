0d6cffd19e41d063c980586636ecf892fdb08193
stangen <31017977+stangen@users.noreply.github.com>
2018-06-27 15:46:12 -0700

    Add files via upload

--- EFA/efa_files/Plotting/check_2_netcdfs.py
+++ EFA/efa_files/Plotting/check_2_netcdfs.py
@@ -39,17 +39,18 @@ from efa_xray.observation.observation import Observation
 from efa_xray.assimilation.ensrf import EnSRF
 
 # Filepath of the precip analysis
-orog1_path = '/home/disk/hot/stangen/Documents/prior_ensembles/eccc/201304/2013-04-01_00_eccc_T2M_ALT.nc'
+orog1_path = '/home/disk/hot/stangen/Documents/prior_ensembles/ecmwf/201304/2013-04-10_00_ecmwf_T2M_ALT.nc'
 
 # Filepath of the observed (analysis)
-orog2_path = '/home/disk/hot/stangen/Documents/prior_ensembles/eccc/201304/2013-04-01_00_eccc_T2M_ALTneweq.nc'
-                
+orog2_path = '/home/disk/hot/stangen/Documents/posterior_ensembles/ob_update_self/loc_1000/ecmwf/201304/2013-04-10_00_ecmwf_T2M_ALT.nc'
+  
+orog3_path = '/home/disk/hot/stangen/Documents/prior_ensembles/ecmwf/201304/2013-04-10_12_ecmwf_T2M_ALT.nc'             
 # only variable I am using is 500mb height            
-vrbls=['ALT']
+vrbls=['T2M']
 
 # Load the prior data            
 with Dataset(orog1_path,'r') as ncdata:
-    print(ncdata.variables)
+    #print(ncdata.variables)
     times = ncdata.variables['time']
     ftimes = num2date(times[:],times.units)
     lats = ncdata.variables['lat'][:]
@@ -62,30 +63,44 @@ with Dataset(orog1_path,'r') as ncdata:
 # Load the analysis data
 with Dataset(orog2_path, 'r') as ncdata:
     orog2 = ncdata.variables[vrbls[0]][:] 
+    times2 = ncdata.variables['time']
+
 #    T500_post = ncdata.variables['T500'][:]
 #    T2M_post = ncdata.variables['T2M'][:]
-    
-difference_orog= orog2[0,:,:,0]-orog2[0,:,:,0]
+with Dataset(orog3_path, 'r') as ncdata:
+    orog3 = ncdata.variables[vrbls[0]][:] 
+    times3 = ncdata.variables['time']
+
+prior_mean = np.mean(orog1,axis=3)    
+post_mean = np.mean(orog2,axis=3)
+analysis_mean = np.mean(orog3,axis=3)
+post_prior_diff= post_mean[1,:,:]-prior_mean[1,:,:]
+anal_prior_diff = prior_mean[1,:,:]-analysis_mean[0,:,:]
+anal_post_diff = post_mean[1,:,:]-analysis_mean[0,:,:]
+
 
 # Plot the difference between the prior and posterior
-map = Basemap(projection='ortho',lat_0=45,lon_0=-100,resolution='l')
+#map = Basemap(projection='ortho',lat_0=45,lon_0=-100,resolution='l')
+map = Basemap(projection='merc',llcrnrlat=25,urcrnrlat=35,\
+            llcrnrlon=-105,urcrnrlon=-95,lat_ts=30,resolution='c')
 # draw coastlines, country boundaries, fill continents.
 map.drawcoastlines(linewidth=0.25)
 map.drawcountries(linewidth=0.25)
 #map.fillcontinents(color='coral',lake_color='aqua')
 # draw the edge of the map projection region (the projection limb)
 map.drawmapboundary(fill_color='aqua')
+#map.fillcontinents(color='coral',lake_color='aqua')
 # draw lat/lon grid lines every 30 degrees.
-map.drawmeridians(np.arange(0,360,30))
-map.drawparallels(np.arange(-90,90,30))
+map.drawmeridians(np.arange(0,360,1))
+map.drawparallels(np.arange(-90,90,1))
 
 # compute native map projection coordinates of lat/lon grid.
 lon, lat = np.meshgrid(lons, lats)
 x, y = map(lon, lat)
 
-time_ind = 0
+time_ind = 2
 #cs = map.contourf(x,y,Z500_diff[0,:,:,0])#,levels=np.arange(-90, 91, 30), cmap=plt.cm.RdBu_r,linewidths=1.5, extend='both')
-cs = map.contourf(x,y,difference_orog)
+cs = map.contourf(x,y,post_prior_diff[:,:])
 # Add Colorbar
 cbar = map.colorbar(cs, location='bottom', pad="10%")
 cbar.set_label(orog1_units)
