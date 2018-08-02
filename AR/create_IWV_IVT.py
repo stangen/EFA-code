be187bee24be47244ce86a68dd6edf981563cb0a
stangen <31017977+stangen@users.noreply.github.com>
2018-08-02 16:58:37 -0700

    Add files via upload

--- AR/create_IWV_IVT.py
+++ AR/create_IWV_IVT.py
@@ -17,8 +17,8 @@ import os
 # ecmwf, eccc for euro/canadian ensembles, ncep
 ensemble_type = ['eccc']
 #start and end date to get ensembles. 
-start_date = datetime(2015,11,10,0) #YYYY,m,d,h
-end_date = datetime(2015,11,10,0)
+start_date = datetime(2015,11,10,12) #YYYY,m,d,h
+end_date = datetime(2015,11,10,12)
 hourstep = 12 #how often you want a new forecast initialization, usually 12 hr
 
 #variables with names coming from the raw TIGGE- see get_tigge_data if unsure of names.
@@ -26,13 +26,7 @@ hourstep = 12 #how often you want a new forecast initialization, usually 12 hr
 surf_variables = ['D2M','SP','U10','V10']
 upper_variables = ['Q','U','V']
 levels = ['1000','925','850','700','500','300']
-end_variables = ['IVT','IWV']
-
-#sfc for surface, pl for elevated
-lev = 'sfc'
-
-#variables with names I want to have after it is processed
-variables = ['TCW']#['ALT','T2M']##, 'P6HR', 'TCW']
+end_variables = ['IWV','IVT','D-IVT']
 
 #a list of dates to loop through to load each forecast initialized on these dates
 dates = mt.make_datetimelist(start_date,end_date,hourstep)  
@@ -42,6 +36,8 @@ upper_str = ef.var_string(upper_variables)+'_'+ef.var_string(levels)+'_pl.nc'
 nvars = len(end_variables)
 
 g = 9.80665
+
+outvar_string = ef.var_string(end_variables)
 
 """
 This function creates a netCDF from a raw TIGGE netCDF. The main purpose
@@ -53,10 +49,7 @@ raw TIGGE int16 format.
 for ens in ensemble_type:
     for date in dates:
 
-        y = date.strftime('%Y')
-        m = date.strftime('%m')
-        d = date.strftime('%d')
-        h = date.strftime('%H')
+        y,m,d,h = ef.dt_str_timedelta(date)
         
         print('Working on '+d+'_'+h+' '+ens)
         
@@ -145,11 +138,10 @@ for ens in ensemble_type:
             q_aloft = aloft.variables['q'][:,:,i,:,:]
             u_aloft = aloft.variables['u'][:,:,i,:,:]
             v_aloft = aloft.variables['v'][:,:,i,:,:]
-            wind_aloft = np.sqrt(u_aloft**2+v_aloft**2)
-
             
    #--------first time through the loop: we need to get the stuff less than 1000 hPa
             if i == 0:
+                print('Working on surface pressure < 1000 hPa')
                 #make array of boolean where surface pressure is greater than 1000 hPa.
                 #this will create an array of the same shape of the ensemble, with
                 #True where the condition is met, and False otherwise. 
@@ -161,14 +153,15 @@ for ens in ensemble_type:
                 q_mean = np.add(q_surf,q_aloft,where=p_surf_less_1000)/2
                 IWV = np.multiply(q_mean,dp,where=p_surf_less_1000)/g
                 
-                state[1,:,:,:,:] = np.add(state[1,:,:,:,:],IWV,where=p_surf_less_1000)
-                #calculate IVT of this column
-                wind_mean = np.add(wind_surf,wind_aloft,where=p_surf_less_1000)/2
-                IVT = np.multiply(IWV,wind_mean,where=p_surf_less_1000)
-                
-                state[0,:,:,:,:] = np.add(state[0,:,:,:,:],IVT,where=p_surf_less_1000)
+                state[0,:,:,:,:] = np.add(state[0,:,:,:,:],IWV)
+                #calculate IVT of this column                
+                u_wind_mean = np.add(u_surf,u_aloft,where=p_surf_less_1000)/2
+                v_wind_mean = np.add(v_surf,v_aloft,where=p_surf_less_1000)/2                
+                u_IVT = np.multiply(IWV,u_wind_mean,where=p_surf_less_1000)
+                v_IVT = np.multiply(IWV,v_wind_mean,where=p_surf_less_1000)
                
             else:
+                print('Working on',lev)
                 p_aloft_lower = int(levels[i-1])*100
                 #if the surface pressure is less than both layers, no need to add
                 #for that gridbox, since the pressure levels are below ground there.
@@ -177,29 +170,78 @@ for ens in ensemble_type:
                 p_surf_between = (p_surf >= p_aloft) & (p_surf < p_aloft_lower)
                 #average surface values with values of upper layer, and get 0s
                 #where this isn't the case
-                dp = np.subtract(p_surf,p_aloft,where=p_surf_between)
+                dp_between = np.subtract(p_surf,p_aloft,where=p_surf_between)
                 q_mean = np.add(q_surf,q_aloft,where=p_surf_between)/2
-                IWV_between = np.multiply(q_mean,dp,where=p_surf_between)/g               
-                state[1,:,:,:,:] = np.add(state[1,:,:,:,:],IWV_between,where=p_surf_between)
+                IWV_between = np.multiply(q_mean,dp_between,where=p_surf_between)/g               
+                state[0,:,:,:,:] = np.add(state[0,:,:,:,:],IWV_between)
                 
-                wind_mean_between = np.add(wind_surf,wind_aloft,where=p_surf_between)
-                IVT_between = np.multiply(IWV_between,wind_mean_between,where=p_surf_between)
-                state[0,:,:,:,:] = np.add(state[0,:,:,:,:],IVT_between,where=p_surf_between)
+                u_wind_mean_between = np.add(u_surf,u_aloft,where=p_surf_between)/2
+                v_wind_mean_between = np.add(v_surf,v_aloft,where=p_surf_between)/2
+                u_IVT_between = np.multiply(IWV_between,u_wind_mean_between,where=p_surf_between)
+                v_IVT_between = np.multiply(IWV_between,v_wind_mean_between,where=p_surf_between)
                 
     #-----------if surface pressure is greater than both pressure levels-
-                #the two layers are both above ground                
+                #the two layers are both above ground               
                 p_surf_greater = (p_surf > p_aloft) & (p_surf > p_aloft_lower)
                 
                 #dp is a scalar in this case
-                dp = p_aloft_lower-p_aloft
+                dp_aloft = p_aloft_lower-p_aloft
                 q_aloft_lower = aloft.variables['q'][:,:,i-1,:,:]
                 q_mean = np.add(q_aloft_lower,q_aloft,where=p_surf_greater)/2
-                IWV_greater = np.multiply(q_mean,dp,where=p_surf_greater)/g
-                state[1,:,:,:,:] = np.add(state[1,:,:,:,:],IWV_greater,where=p_surf_greater)
+                IWV_greater = np.multiply(q_mean,dp_aloft,where=p_surf_greater)/g
+                state[0,:,:,:,:] = np.add(state[0,:,:,:,:],IWV_greater)
                 
-                wind_aloft_lower = np.sqrt(aloft.variables['u'][:,:,i-1,:,:]**2+aloft.variables['v'][:,:,i-1,:,:]**2)
-                wind_mean_greater = np.add(wind_aloft_lower,wind_aloft,where=p_surf_greater)
-                IVT_greater = np.multiply(IWV_greater,wind_mean_greater,where=p_surf_greater)
-                state[0,:,:,:,:] = np.add(state[0,:,:,:,:],IVT_greater,where=p_surf_greater)
+                u_aloft_lower = aloft.variables['u'][:,:,i-1,:,:]
+                v_aloft_lower = aloft.variables['v'][:,:,i-1,:,:]
                 
+                u_wind_mean_greater = np.add(u_aloft_lower,u_aloft,where=p_surf_greater)/2
+                v_wind_mean_greater = np.add(v_aloft_lower,v_aloft,where=p_surf_greater)/2
+                u_IVT_greater = np.multiply(IWV_greater,u_wind_mean_greater,where=p_surf_greater)
+                v_IVT_greater = np.multiply(IWV_greater,v_wind_mean_greater,where=p_surf_greater)     
+                
+                #add the IVT from each category of surface pressure to layer pressure
+                u_IVT = u_IVT + u_IVT_between + u_IVT_greater
+                v_IVT = v_IVT + v_IVT_between + v_IVT_between
+                                
+        #find magnitude of IVT 
+        state[1,:,:,:,:] = np.sqrt(u_IVT**2+v_IVT**2)
+        #find direction of IVT
+        state[2,:,:,:,:] = np.arctan2(v_IVT,u_IVT)*180/np.pi
+        
+        #get state so that it is nvars x ntimes x nlats x nlons x nmems
+        state = np.swapaxes(state, 2, 3)
+        state = np.swapaxes(state, 3, 4)
+        
+        print('Writing to netcdf...')
+        # Convert times back to integers
+        valid_times = date2num(ftimes,tunit)
+        
+        # Write ensemble forecast to netcdf - change name here
+        #dset = Dataset(outdir+y+'-'+m+'-'+d+'_'+h+'_'+ens_type+'_'+outvar_string+'.nc','w')
+        dset = Dataset(outdir+y+'-'+m+'-'+d+'_'+h+'_'+tr_str+'_'+outvar_string+'.87nc','w')
+        dset.createDimension('time',None)
+        dset.createDimension('lat',nlats)
+        dset.createDimension('lon',nlons)
+        dset.createDimension('ens',nmems)
+        dset.createVariable('time','i4',('time',))
+        dset.createVariable('lat',np.float32,('lat',))
+        dset.createVariable('lon',np.float32,('lon'))
+        dset.createVariable('ens','i4',('ens',))
+        dset.variables['time'].units = tunit
+        dset.variables['lat'].units = 'degrees_north'
+        dset.variables['lon'].units = 'degrees_east'
+        dset.variables['ens'].units = 'member_number'
+        dset.variables['time'][:] = np.array(valid_times)
+        dset.variables['lat'][:] = lats
+        dset.variables['lon'][:] = lons
+        dset.variables['ens'][:] = memarr
+        for v,var in enumerate(end_variables):
+            #var = vardict[var]
+            print('Writing variable {}'.format(var))
+            dset.createVariable(var, np.float32, ('time','lat','lon','ens',))
+            dset.variables[var].units = ef.get_units(var)
+            dset.variables[var][:] = state[v,:,:,:,:]
+        #completes writing the file
+        dset.close()
+
         
